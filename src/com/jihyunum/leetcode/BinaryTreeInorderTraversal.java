48f0341c6db7069633e0129e1c05563f2e5c951a
jihyun-um <ashleyum20@gmail.com>
2019-12-10 07:11:06 +0000

    Add solution for binary tree inorder traversal

---
+++ src/com/jihyunum/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,50 @@
+package com.jihyunum.leetcode;
+
+import com.jihyunum.leetcode.datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+// [Problem] https://leetcode.com/problems/binary-tree-inorder-traversal/
+class BinaryTreeInorderTraversal {
+
+    // Recursion solution - O(N) time, O(logN) space
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> values = new ArrayList<>();
+        addValueInorder(root, values);
+        return values;
+    }
+
+    private void addValueInorder(TreeNode node, List<Integer> values) {
+        if (node != null) {
+            if (node.left != null) {
+                addValueInorder(node.left, values);
+            }
+            values.add(node.val);
+            if (node.right != null) {
+                addValueInorder(node.right, values);
+            }
+        }
+    }
+
+    // Stack solution - O(N) time, O(N) space
+    public List<Integer> inorderTraversalStack(TreeNode root) {
+        List<Integer> values = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode currentNode = root;
+        while (currentNode != null || !stack.isEmpty()) {
+            while (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            }
+            currentNode = stack.pop();
+            values.add(currentNode.val);
+            currentNode = currentNode.right;
+        }
+
+        return values;
+    }
+
+}
