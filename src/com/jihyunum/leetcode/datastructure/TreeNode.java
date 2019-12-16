320c07633c7d927e83f494f9216f000e19d0914f
jihyun-um <ashleyum20@gmail.com>
2019-12-16 08:39:43 +0000

    Add preOrder, inOrder, postOrder implementations to TreeNode

--- src/com/jihyunum/leetcode/datastructure/TreeNode.java
+++ src/com/jihyunum/leetcode/datastructure/TreeNode.java
@@ -8,4 +8,34 @@ public class TreeNode {
     public TreeNode(int x) {
         val = x;
     }
+
+    public void printPreOrder(TreeNode node) {
+        System.out.println(node.val);
+        if (node.left != null) {
+            printPreOrder(node.left);
+        }
+        if (node.right != null) {
+            printPreOrder(node.right);
+        }
+    }
+
+    public void printInOrder(TreeNode node) {
+        if (node.left != null) {
+            printInOrder(node.left);
+        }
+        System.out.println(node.val);
+        if (node.right != null) {
+            printInOrder(node.right);
+        }
+    }
+
+    public void printPostOrder(TreeNode node) {
+        if (node.left != null) {
+            printPostOrder(node.left);
+        }
+        if (node.right != null) {
+            printPostOrder(node.right);
+        }
+        System.out.println(node.val);
+    }
 }
