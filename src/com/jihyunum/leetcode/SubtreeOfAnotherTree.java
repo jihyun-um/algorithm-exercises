45628b7540eef7064196c00fe95c24ef9ba3f31a
jihyun-um <ashleyum20@gmail.com>
2020-06-14 22:22:27 +0900

    Add solution for subtree of another tree

---
+++ src/com/jihyunum/leetcode/SubtreeOfAnotherTree.java
@@ -0,0 +1,54 @@
+package com.jihyunum.leetcode;
+
+import com.jihyunum.leetcode.datastructure.TreeNode;
+
+// [Problem] https://leetcode.com/problems/subtree-of-another-tree/
+class SubtreeOfAnotherTree {
+
+    // test
+    public static void main(String[] args) {
+
+        SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+
+        // Given tree s:
+        //      3
+        //    / \
+        //   4   5
+        //  / \
+        // 1   2
+        TreeNode s = new TreeNode(3,
+                new TreeNode(4, new TreeNode(1), new TreeNode(2)), new TreeNode(5));
+
+        // Given tree t:
+        //   4
+        //  / \
+        // 1   2
+        TreeNode t = new TreeNode(4, new TreeNode(1), new TreeNode(2));
+
+        boolean expectedOutput = true;
+        boolean actualOutput = solution.isSubtree(s, t);
+
+        System.out.println("Test passed? " + (expectedOutput == actualOutput));
+    }
+
+    // Preorder Traversal and Recursion - O(m * n) time, O(n) space
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return isSameTree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
