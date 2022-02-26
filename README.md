b744d27976b319d02ac829754c05c8d89f2f03bc
jihyun-um <ashleyum20@gmail.com>
2022-02-26 16:08:52 +0000

    Add solution for Island Perimeter

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ Algorithm exercises from LeetCode implemented in Java (v11) and JavaScript.
 ### Matrix
 - Max Increase to Keep City Skyline | [Problem](https://leetcode.com/problems/max-increase-to-keep-city-skyline) | [Java Solution](src/javacode/solutions/MaxIncreaseToKeepCitySkyline.java)
 - Cells with Odd Values in a Matrix | [Problem](https://leetcode.com/problems/cells-with-odd-values-in-a-matrix) | [Java Solution](src/javacode/solutions/OddCellsInMatrix.java)
+- Island Perimeter | [Problem](https://leetcode.com/problems/island-perimeter) | [Java Solution](src/javacode/solutions/IslandPerimeter.java)
 
 ### HashTable
 - Two Sum | [Problem](https://leetcode.com/problems/two-sum) | [Java Solution](src/javacode/solutions/TwoSum.java) | [JS Solution](src/javascript/solutions/twoSum.js)
---
+++ src/javacode/solutions/IslandPerimeter.java
@@ -0,0 +1,91 @@
+package javacode.solutions;
+
+// [Problem] https://leetcode.com/problems/island-perimeter
+class IslandPerimeter {
+    // Matrix simple counting
+    // O(mn) time, where m = row length, n = column length
+    // O(1) space
+    public int islandPerimeter(int[][] grid) {
+        int rowLength = grid.length, colLength = grid[0].length;
+        int perimeter = 0;
+        for (int row = 0; row < rowLength; row++) {
+            for (int col = 0; col < colLength; col++) {
+                if (grid[row][col] == 1) {
+                    if (row == 0 || grid[row - 1][col] == 0) { // top
+                        perimeter++;
+                    }
+                    if (row == rowLength - 1 || grid[row + 1][col] == 0) { // bottom
+                        perimeter++;
+                    }
+                    if (col == 0 || grid[row][col - 1] == 0) { // left
+                        perimeter++;
+                    }
+                    if (col == colLength - 1 || grid[row][col + 1] == 0) { // right
+                        perimeter++;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+
+    // Matrix neighbor counting
+    // O(mn) time, O(1) space
+    public int islandPerimeterNeighborCounting(int[][] grid) {
+        int rowLength = grid.length, colLength = grid[0].length;
+        int islands = 0, neighbors = 0;
+        for (int row = 0; row < rowLength; row++) {
+            for (int col = 0; col < colLength; col++) {
+                if (grid[row][col] == 1) {
+                    islands++;
+                    if (row < rowLength - 1 && grid[row + 1][col] == 1) {
+                        neighbors++; // right neighbor
+                    }
+                    if (col < colLength - 1 && grid[row][col + 1] == 1) {
+                        neighbors++; // bottom neighbor
+                    }
+                }
+            }
+        }
+        return islands * 4 - neighbors * 2;
+    }
+
+    // DFS
+    // O(mn) time, O(1) space
+    public int islandPerimeterDfs(int[][] grid) {
+        int rowLength = grid.length, colLength = grid[0].length;
+        for (int row = 0; row < rowLength; row++) {
+            for (int col = 0; col < colLength; col++) {
+                if (grid[row][col] == 1) {
+                    return getPerimeter(grid, row, col);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int getPerimeter(int[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0) {
+            return 1;
+        }
+        if (grid[row][col] == -1) { // has been visited
+            return 0;
+        }
+        grid[row][col] = -1;
+        return getPerimeter(grid, row - 1, col) // top
+                + getPerimeter(grid, row + 1, col) // bottom
+                + getPerimeter(grid, row, col - 1) // left
+                + getPerimeter(grid, row, col + 1); // right
+    }
+
+    // Test
+    public static void main(String[] args) {
+        IslandPerimeter solution = new IslandPerimeter();
+
+        int[][] input = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        int expectedOutput = 16;
+        int actualOutput = solution.islandPerimeter(input);
+
+        System.out.println("Test passed? " + (expectedOutput == actualOutput));
+    }
+}
