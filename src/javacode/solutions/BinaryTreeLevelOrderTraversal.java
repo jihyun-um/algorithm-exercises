7b6c59b1af780f15068dd219bc3e252430ec804f
jihyun-um <ashleyum20@gmail.com>
2022-03-11 21:12:01 +0000

    Update solution for Binary Tree Level Order Traversal

--- src/javacode/solutions/BinaryTreeLevelOrderTraversal.java
+++ src/javacode/solutions/BinaryTreeLevelOrderTraversal.java
@@ -7,56 +7,52 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-// [Problem] https://leetcode.com/problems/binary-tree-level-order-traversal/
+// [Problem] https://leetcode.com/problems/binary-tree-level-order-traversal
 class BinaryTreeLevelOrderTraversal {
+    // BFS
+    // O(n) time, O(n) space
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) {
+            return levels;
+        }
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            int currentLevelSize = nodes.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode node = nodes.poll();
+                currentLevel.add(node.val);
+                if (node.left != null) {
+                    nodes.add(node.left);
+                }
+                if (node.right != null) {
+                    nodes.add(node.right);
+                }
+            }
+            levels.add(currentLevel);
+        }
+        return levels;
+    }
 
-    // test
+    // Test
     public static void main(String[] args) {
         BinaryTreeLevelOrderTraversal solution = new BinaryTreeLevelOrderTraversal();
 
-        // Input: [3, 9, 20, null, null, 15, 7]
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        root.right = new TreeNode(20);
-        root.right.left = new TreeNode(15);
-        root.right.right = new TreeNode(7);
+        // Given input tree:
+        //     3
+        //    / \
+        //   9   20
+        //      /  \
+        //     15   7
+        TreeNode root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7)));
 
-        // Output:
-        // [
-        //  [3],
-        //  [9,20],
-        //  [15,7]
-        // ]
-        List<List<Integer>> output = solution.levelOrder(root);
-        System.out.println(output);
+        List<List<Integer>> expectedOutput = List.of(List.of(3), List.of(9, 20), List.of(15, 7));
+        List<List<Integer>> actualOutput = solution.levelOrder(root);
+
+        System.out.println("Test passed? " + expectedOutput.equals(actualOutput));
     }
-
-    // BFS Solution
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> leveledValues = new ArrayList<>();
-        if (root == null) {
-            return leveledValues;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            System.out.println(queue);
-            int queueSize = queue.size();
-            List<Integer> currentLevel = new ArrayList<>();
-            for (int i = 0; i < queueSize; i++) {
-                TreeNode currentNode = queue.poll();
-                currentLevel.add(currentNode.val);
-                if (currentNode.left != null) {
-                    queue.add(currentNode.left);
-                }
-                if (currentNode.right != null) {
-                    queue.add(currentNode.right);
-                }
-            }
-            leveledValues.add(currentLevel);
-        }
-        return leveledValues;
-    }
-
 }
