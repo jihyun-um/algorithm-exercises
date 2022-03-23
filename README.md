330c261bbf112db2d44c2b4e065d4756b5542096
jihyun-um <ashleyum20@gmail.com>
2022-03-23 22:56:12 +0000

    Add solution for Maximum Product of Split Binary Tree

--- README.md
+++ README.md
@@ -85,6 +85,7 @@ Algorithm exercises from LeetCode implemented in Java (v11) and JavaScript.
 - All Paths From Source to Target | [Problem](https://leetcode.com/problems/all-paths-from-source-to-target) | [Java Solution](src/javacode/solutions/AllPathsFromSourceToTarget.java)
 - Count Univalue Subtrees | [Problem](https://leetcode.com/problems/count-univalue-subtrees) | [Java Solution](src/javacode/solutions/CountUnivalueSubtrees.java)
 - Delete Nodes And Return Forest | [Problem](https://leetcode.com/problems/delete-nodes-and-return-forest) | [Java Solution](src/javacode/solutions/DeleteNodesAndReturnForest.java)
+- Maximum Product of Split Binary Tree | [Problem](https://leetcode.com/problems/maximum-product-of-splitted-binary-tree) | [Java Solution](src/javacode/solutions/MaximumProductOfSplitBinaryTree.java)
 - Number of Islands | [Problem](https://leetcode.com/problems/number-of-islands) | [Java Solution](src/javacode/solutions/NumberOfIslands.java)
 
 ### Backtracking
---
+++ src/javacode/solutions/MaximumProductOfSplitBinaryTree.java
@@ -0,0 +1,66 @@
+package javacode.solutions;
+
+import javacode.datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// [Problem] https://leetcode.com/problems/maximum-product-of-splitted-binary-tree
+class MaximumProductOfSplitBinaryTree {
+    List<Long> allSums = new ArrayList<>();
+
+    // DFS
+    // O(n) time, O(n) space
+    public int maxProduct(TreeNode root) {
+        long totalSum = calculateTreeSum(root);
+        long maxProduct = 0;
+        for (long sum : allSums) {
+            maxProduct = Math.max(sum * (totalSum - sum), maxProduct);
+        }
+        return (int) (maxProduct % 1000000007);
+    }
+
+    private long calculateTreeSum(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        long treeSum = node.val + calculateTreeSum(node.left) + calculateTreeSum(node.right);
+        allSums.add(treeSum);
+        return treeSum;
+    }
+
+    // Test
+    public static void main(String[] args) {
+        MaximumProductOfSplitBinaryTree removeHistory = new MaximumProductOfSplitBinaryTree();
+
+        // Given input tree:
+        //      1
+        //    /   \
+        //   2     3
+        //  / \   /
+        // 4   5 6
+        TreeNode input1 = new TreeNode(1,
+                new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                new TreeNode(3, new TreeNode(6), null));
+        int expectedOutput1 = 110;
+        int actualOutput1 = removeHistory.maxProduct(input1);
+        System.out.println("Test 1 passed? " + (expectedOutput1 == actualOutput1));
+
+        // Given input tree:
+        //      1
+        //       \
+        //        2
+        //       / \
+        //      3   4
+        //         / \
+        //        5   6
+        TreeNode input2 = new TreeNode(1,
+                null,
+                new TreeNode(2,
+                        new TreeNode(3),
+                        new TreeNode(4, new TreeNode(5), new TreeNode(6))));
+        int expectedOutput2 = 110;
+        int actualOutput2 = removeHistory.maxProduct(input2);
+        System.out.println("Test 2 passed? " + (expectedOutput2 == actualOutput2));
+    }
+}
