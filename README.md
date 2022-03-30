d0b26b7219e59122fceb494e3966e6e9c95bb7c4
jihyun-um <ashleyum20@gmail.com>
2022-03-30 21:23:28 +0100

    Add solution for Valid Sudoku

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ Algorithm exercises from LeetCode implemented in Java (v11) and JavaScript.
 - Max Increase to Keep City Skyline | [Problem](https://leetcode.com/problems/max-increase-to-keep-city-skyline) | [Java Solution](src/javacode/solutions/MaxIncreaseToKeepCitySkyline.java)
 - Cells with Odd Values in a Matrix | [Problem](https://leetcode.com/problems/cells-with-odd-values-in-a-matrix) | [Java Solution](src/javacode/solutions/OddCellsInMatrix.java)
 - Island Perimeter | [Problem](https://leetcode.com/problems/island-perimeter) | [Java Solution](src/javacode/solutions/IslandPerimeter.java)
+- Valid Sudoku | [Problem](https://leetcode.com/problems/valid-sudoku) | [Java Solution](src/javacode/solutions/ValidSudoku.java)
 
 ### HashTable
 - Two Sum | [Problem](https://leetcode.com/problems/two-sum) | [Java Solution](src/javacode/solutions/TwoSum.java) | [JS Solution](src/javascript/solutions/twoSum.js)
---
+++ src/javacode/solutions/ValidSudoku.java
@@ -0,0 +1,67 @@
+package javacode.solutions;
+
+// [Problem] https://leetcode.com/problems/valid-sudoku
+class ValidSudoku {
+    // Using 2d arrays for frequency counting
+    // O(n^2) time, O(n^2) space
+    // but since n is always 9, it is O(1)
+    public boolean isValidSudoku(char[][] board) {
+        final int N = 9;
+        int[][] rowFrequencies = new int[N][N];
+        int[][] colFrequencies = new int[N][N];
+        int[][] boxFrequencies = new int[N][N];
+
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                int[] rowFrequency = rowFrequencies[row];
+                int[] colFrequency = colFrequencies[col];
+                int box = (row / 3) * 3 + (col / 3);
+                int[] boxFrequency = boxFrequencies[box];
+
+                char cell = board[row][col];
+                if (cell == '.') {
+                    continue;
+                }
+                if (rowFrequency[cell - '1']++ > 0 || colFrequency[cell - '1']++ > 0 || boxFrequency[cell - '1']++ > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Test
+    public static void main(String[] args) {
+        ValidSudoku solution = new ValidSudoku();
+
+        char[][] input1 = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        boolean expectedOutput1 = true;
+        boolean actualOutput1 = solution.isValidSudoku(input1);
+        System.out.println("Test 1 passed? " + (expectedOutput1 == actualOutput1));
+
+        char[][] input2 = {
+                {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        boolean expectedOutput2 = false;
+        boolean actualOutput2 = solution.isValidSudoku(input2);
+        System.out.println("Test 2 passed? " + (expectedOutput2 == actualOutput2));
+    }
+}
